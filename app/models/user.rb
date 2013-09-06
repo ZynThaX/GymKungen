44ddf213fda7947f282de9630541f5979330ef32
Jonas Jinbäck <jonasss.90@gmail.com>
2013-09-06 16:24:03 +0200

    added gem settings

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,18 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id                              :integer          not null, primary key
+#  email                           :string(255)      not null
+#  crypted_password                :string(255)
+#  salt                            :string(255)
+#  created_at                      :datetime         not null
+#  updated_at                      :datetime         not null
+#  reset_password_token            :string(255)
+#  reset_password_token_expires_at :datetime
+#  reset_password_email_sent_at    :datetime
+#
+
 class User < ActiveRecord::Base
   authenticates_with_sorcery!
   # attr_accessible :title, :body
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,3 +1,18 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id                              :integer          not null, primary key
+#  email                           :string(255)      not null
+#  crypted_password                :string(255)
+#  salt                            :string(255)
+#  created_at                      :datetime         not null
+#  updated_at                      :datetime         not null
+#  reset_password_token            :string(255)
+#  reset_password_token_expires_at :datetime
+#  reset_password_email_sent_at    :datetime
+#
+
 require 'spec_helper'
 
 describe User do
